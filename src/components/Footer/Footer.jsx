9ebc7ceaac06e8d865a0f2dbc0e4cd91994cdf1c
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2022-11-23 14:06:45 +0400

    Added build, changed icon and title, added footer with link

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>made by</p>
+      <a href="https://vshevchenko.netlify.app/" target="_blank">Valerii Shevchenko</a> 
+    </div>
+  )
+}
+
+export default Footer
